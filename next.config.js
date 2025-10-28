7163a451794f1c3acaa7e8cd2d5f7cc54064df12
dhruvshivhare1 <dhruvshivhare16@gmail.com>
2025-10-29 03:46:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['vercel.com'],
+    domains: ['vercel.com', 'res.cloudinary.com'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     formats: ['image/webp'],
